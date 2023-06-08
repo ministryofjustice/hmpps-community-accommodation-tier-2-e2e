5de6fe0202391c3eb47081f9be4405a133286da8
Patrick Fleming <patrick.fleming@dwx.com>
2023-06-08 14:31:49 +0100

    Add baseURL to config
    so we no longer need to specify the entire URL when visiting pages.

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -3,7 +3,7 @@ import { test as setup } from '@playwright/test'
 const authFile = 'playwright/.auth/user.json'
 
 setup('authenticate', async ({ page }) => {
-  await page.goto('https://supported-accommodation-dev.hmpps.service.justice.gov.uk')
+  await page.goto('/')
   await page.getByLabel('Username').fill(process.env.HMPPS_AUTH_USERNAME as string)
   await page.getByLabel('Password').fill(process.env.HMPPS_AUTH_PASSWORD as string)
   await page.getByRole('button', { name: 'Sign in' }).click()
