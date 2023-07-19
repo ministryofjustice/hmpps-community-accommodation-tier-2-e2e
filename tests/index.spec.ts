08c7d1e8d62fea1abe40777ecc0b652b76d486f8
Patrick Fleming <patrick.fleming@dwx.com>
2023-07-19 16:26:30 +0100

    Remove index.spec.ts
    …as it was experimental and has now been replaced by more useful test code.

--- tests/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('has a title', async ({ page }) => {
-  await page.goto('/')
-
-  await expect(page.locator('h1')).toHaveText('Apply for a CAS-2 placement')
-})
