49fc371cb5b5b714b8424c220a7235e941d8ca1d
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-07-27 11:40:55 +0100

    Fix E2E tests for separate 'Find by CRN' form
    The 'apply' journey test was broken due to the introduction
    of a separate page for the "Enter a CRN" form.
    
    I've taken the opportunity to try to make the tests clearer
    by renaming some helper "page" classes and other
    refactorings:
    
    STRUCTURE OF 'APPLY' FEATURE
    
    - `startAnApplication(page)`
      navigate to the start of the journey
    
    - `enterCrn(page, person.crn)`
      search for a person
    
    - `completeFundingInformationTask(page, person.name)`:
      complete a task
    
    we'll shortly add another "completeTask" step:
    
    - `completeEqualityAndDiversityTask(page, person.name)`
    
    "PAGE" HELPER NAMES
    
    - `StartPage` is used to navigate to and from the  homepage /
    root url
    
    - `applicationsDashboardPage` is the 'index' or list of
    applications, which we anticipate developing into an
    important 'dashboard'

--- pages/apply/index.ts
+++ pages/apply/index.ts
@@ -1,5 +1,7 @@
 import { CRNPage } from './crnPage'
 import { TaskListPage } from './taskListPage'
 import { ApplyPage } from './applyPage'
+import { StartPage } from './startPage'
+import { ApplicationsDashboardPage } from './applicationsDashboardPage'
 
-export { CRNPage, TaskListPage, ApplyPage }
+export { CRNPage, TaskListPage, ApplyPage, StartPage, ApplicationsDashboardPage }
