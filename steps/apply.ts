25502492e2b2b272830f37aba0bcb375b70f4a15
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-07-31 14:20:57 +0100

    Complete the 'About the person' section
    This new section is comprised of one task (with one page)
    initially.
    
    We follow the link from the task list ("Complete equality and diversity monitoring")
    to the first page and indicate that "Yes" the person is
    willing to answer the equality and diversity questions.

--- steps/apply.ts
+++ steps/apply.ts
@@ -35,3 +35,19 @@ export const completeFundingInformationTask = async (page: Page, name: string) =
   await fundingInformationPage.checkRadio('Personal money or savings')
   await fundingInformationPage.clickSave()
 }
+
+export const completeAboutThePersonSection = async (page: Page, name: string) => {
+  await completeWillAnswerEqualityQuestionsTask(page, name)
+}
+
+const completeWillAnswerEqualityQuestionsTask = async (page: Page, name: string) => {
+  const taskListPage = new TaskListPage(page)
+  await taskListPage.clickTask('Complete equality and diversity monitoring')
+
+  const willAnswerEqualityQuestionsPage = await ApplyPage.initialize(
+    page,
+    `Does ${name} want to answer the equality questions?`,
+  )
+  await willAnswerEqualityQuestionsPage.checkRadio('Yes, answer the equality questions (takes 2 minutes)')
+  await willAnswerEqualityQuestionsPage.clickSave()
+}
--- tests/apply.spec.ts
+++ tests/apply.spec.ts
@@ -1,13 +1,16 @@
 import { expect } from '@playwright/test'
 import { test } from '../test'
-import { completeFundingInformationTask, enterCrn, startAnApplication } from '../steps/apply'
+import {
+  completeFundingInformationTask,
+  completeAboutThePersonSection,
+  enterCrn,
+  startAnApplication,
+} from '../steps/apply'
 
-test('create a CAS-2 application with funding information', async ({ page, person }) => {
+test('create a CAS-2 application', async ({ page, person }) => {
   await startAnApplication(page)
-
   await enterCrn(page, person.crn)
-
   await completeFundingInformationTask(page, person.name)
-
-  await expect(page.getByText('You have completed 1 of 2 sections')).toBeVisible()
+  await completeAboutThePersonSection(page, person.name)
+  await expect(page.getByText('You have completed 2 of 2 sections')).toBeVisible()
 })
