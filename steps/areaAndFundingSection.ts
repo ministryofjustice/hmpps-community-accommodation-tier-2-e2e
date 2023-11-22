9bf1de0e60bb4c00eebe61357cb39c247cc65c28
Daniel Liburd <daniel.liburd@gmail.com>
2023-11-22 15:15:44 +0000

    Add test for completing AreaInformation task

--- steps/areaAndFundingSection.ts
+++ steps/areaAndFundingSection.ts
@@ -21,3 +21,15 @@ async function completeNationalInsurancePage(page: Page, name: string) {
   )
   await willAnswerEqualityQuestionsPage.clickSave()
 }
+
+export const completeAreaInformationTask = async (page: Page, name: string) => {
+  const taskListPage = new TaskListPage(page)
+  await taskListPage.clickTask('Add exclusion zones and preferred areas')
+
+  const areaInformationPage = await ApplyPage.initialize(page, `First preferred area for ${name}`)
+
+  await areaInformationPage.fillField('Preferred area', 'London')
+  await areaInformationPage.fillField('Reason for preference', 'Family')
+
+  await areaInformationPage.clickSave()
+}
