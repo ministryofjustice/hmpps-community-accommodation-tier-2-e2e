b46783f9c75080b4a1c48c2fda0901c2660e2adb
Patrick Fleming <patrick.fleming@dwx.com>
2023-12-11 16:40:15 +0000

    Add consent task tests

--- steps/beforeYouStartSection.ts
+++ steps/beforeYouStartSection.ts
@@ -10,3 +10,18 @@ export const completeEligibilityTask = async (page: Page, name: string) => {
   await confirmEligibilityPage.checkRadio('Yes')
   await confirmEligibilityPage.clickSave()
 }
+
+export const completeConsentTask = async (page: Page, name: string) => {
+  const confirmConsentPage = await ApplyPage.initialize(
+    page,
+    `Confirm ${name}'s consent to apply for Short-Term Accommodation (CAS-2)`,
+  )
+
+  await confirmConsentPage.checkRadio(`Yes, ${name} has given their consent`)
+  await confirmConsentPage.fillDateFieldInGroup('When did they give consent?', {
+    year: '2022',
+    month: '3',
+    day: '1',
+  })
+  await confirmConsentPage.clickSave()
+}
