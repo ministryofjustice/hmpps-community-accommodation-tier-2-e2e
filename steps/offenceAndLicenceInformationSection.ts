4d4e5cf442a0d9db364e1994ac3047a312be1bde
Patrick Fleming <patrick.fleming@dxw.com>
2024-01-17 13:45:23 +0000

    Remove offence number

--- steps/offenceAndLicenceInformationSection.ts
+++ steps/offenceAndLicenceInformationSection.ts
@@ -10,7 +10,7 @@ export const completeCurrentOffencesTask = async (page, name) => {
 
 async function completeCurrentOffenceDetailsPage(page, name) {
   const currentOffenceDetailsPage = await ApplyPage.initialize(page, `Add ${name}'s current offence details`)
-  await currentOffenceDetailsPage.fillField('Offence title and number', 'Stalking (08000)')
+  await currentOffenceDetailsPage.fillField('Offence title', 'Stalking')
   await currentOffenceDetailsPage.chooseSelectItem('Offence category', 'Stalking or Harassment')
   await currentOffenceDetailsPage.fillDateFieldInGroup('When did they commit the offence?', {
     year: '2022',
